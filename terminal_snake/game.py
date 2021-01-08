381ddf8ef6cf44fc862b7d70bd74dbde3e16ea02
Julian Leucker <leuckerj@gmail.com>
2021-01-08 22:36:44 +0100

    Add support for themes

--- terminal_snake/game.py
+++ terminal_snake/game.py
@@ -1,17 +1,21 @@
 from random import randint
 import json
 
+from .theme import Theme, DefaultTheme
+
 
 class Game:
-    def __init__(self, field_size):
+    def __init__(self, field_size: int = 5, theme: Theme = DefaultTheme):
         self.snake = [(field_size // 2 + 1, field_size // 2 + 1)]  # field starts at 1; (1, 1) is the top left corner
         self.field_size = field_size
         self.orientation = 0  # 0 = Up, 1 = Right, 2 = Down, 3 = Left
         self.apples = []
 
+        self.theme = theme
+
         self.max_apples = 1
         self.max_score = self.field_size ** 2
-        self.orientation_switch = {
+        self.orientation_switch = {  # urdl
             0: (0, -1),
             1: (1, 0),
             2: (0, 1),
@@ -29,6 +33,12 @@ class Game:
         except (json.decoder.JSONDecodeError, FileNotFoundError) as e:
             self.highscore = 0
             self.scores = {}
+        # ┏
+        # ━
+        # ┃
+        # ┛
+        # ┗
+        # ┓
 
     def set_apple(self):
         if self.max_score > len(self.apples) + self.score:
@@ -67,23 +77,48 @@ class Game:
             return False
 
     def draw_field(self):
-        print("🟧" * (self.field_size + 2))
+        entire_field = [[self.theme.border_piece] * (self.field_size + 2)]
         for y in range(1, self.field_size + 1):
-            print("🟧", end="")
+            row = [self.theme.border_piece]
             for x in range(1, self.field_size + 1):
                 field = (x, y)
 
                 if field in self.apples:
-                    print("🍎", end="")
-                elif field == self.snake[-1]:
-                    print("😳", end="")
+                    row.append(self.theme.apple)
                 elif field in self.snake:
-                    print("🔲", end="")
+                    row.append("🔲")
                 else:
-                    print("🟫", end="")
+                    row.append(self.theme.field_piece)
 
-            print("🟧")
-        print("🟧" * (self.field_size + 2))
+            row.append(self.theme.border_piece)
+            entire_field.append(row)
+        entire_field.append([self.theme.border_piece] * (self.field_size + 2))
+
+        for index, (x, y) in enumerate(self.snake):
+            if (x, y) == self.snake[-1]:
+                entire_field[y][x] = self.theme.head
+            elif (x, y) == self.snake[0]:
+                entire_field[y][x] = self.theme.tail_end
+            else:
+                prev_x, prev_y = self.snake[index - 1]
+                next_x, next_y = self.snake[index + 1]
+                diff_1 = (prev_x - x, prev_y - y)
+                diff_2 = (next_x - x, next_y - y)
+
+                orientation_1 = list(self.orientation_switch.values()).index(diff_1)
+                orientation_2 = list(self.orientation_switch.values()).index(diff_2)
+
+                string_1 = "urdl"[orientation_1]
+                string_2 = "urdl"[orientation_2]
+
+                icon = string_1 + string_2
+
+                entire_field[y][x] = self.theme.tail[icon]
+
+        for row in entire_field:
+            for item in row:
+                print(item, end=self.theme.character_separator)
+            print()
 
     @property
     def score(self):
@@ -97,6 +132,3 @@ class Game:
             with open("scores.json", "w") as f:
                 self.scores[str(self.field_size)] = self.score
                 f.write(json.dumps(self.scores))
-
-
-
